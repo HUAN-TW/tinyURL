d3777c1af32102d119705135d73ee9f899e46611
HUAN-TW <fb000326@gmail.com>
2023-09-14 14:14:24 +0800

    添加取得submit info

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -7,11 +8,19 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/url', (req, res) => {
+  console.log(req.body)
+  const url = req.body.url
+
+  res.send(`You submitted the URL: ${url}`)
+})
+
 app.listen(port, () => {
-  console.log(`express server is running on http://localhost:${port}`)
+  console.log(`Express server is running on http://localhost:${port}`)
 })
